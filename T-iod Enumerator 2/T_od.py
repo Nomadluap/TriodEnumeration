e252fe14d47e7161c59ec5847e3e5227f8fcc5d5
Paul Belanger <paulirino@gmail.com>
2014-07-04 11:00:54 -0400

    -

--- T-iod Enumerator 2/T_od.py
+++ T-iod Enumerator 2/T_od.py
@@ -6,17 +6,18 @@ Created on Jun 6, 2014
 @author: paul
 '''
 
+
 class Point(tuple):
     '''
-    A class which abstracts a tuple. In basic usage, it is used simply to 
-    overload the __eq__ operator, so that tuple-based points handle the 
-    base point correctly. 
+    A class which abstracts a tuple. In basic usage, it is used simply to
+    overload the __eq__ operator, so that tuple-based points handle the
+    base point correctly.
     '''
     def __init__(self, a, b=None):
         if b is not None:
             a = (a, b)
         tuple.__init__(self, a)
-        
+
     def __new__(self, a, b=None):
         '''
         Redefinition of the tuple method so that points can be constructed
@@ -26,7 +27,7 @@ class Point(tuple):
         if b is not None:
             a = (a, b)
         return tuple.__new__(self, a)
-    
+
     def __eq__(self, y):
         '''
         True if and only if x == y.
@@ -38,23 +39,24 @@ class Point(tuple):
             return True
         else:
             return tuple.__eq__(self, y)
-        
+
     def __sub__(self, y):
         '''
         Defines subtraction between two Points such that the distance between
-        two points is returned, using a railway metric.
+        two points is returned, using a railway metric. The result returned is
+        always in absolute value.
         '''
         #if both points lie on the same arm, then use simple subtraction
         if self[0] == y[0]:
-            return self[1] - y[1]
+            return abs(self[1] - y[1])
         #otherwise return the sum of distances of each point from the center
         else:
             return self[1] + y[1]
-        
+
     def __str__(self):
         '''for easy debugging'''
         return "p" + tuple(self).__str__()
-    
+
     def __repr__(self):
         '''for easy debugging'''
         return "p" + tuple(self).__repr__()
