64468daf43d98f65999d1771d4b39e2e1bcf9da9
Paul Belanger <paulirino@gmail.com>
2014-07-15 13:38:28 -0400

    renamed overseer files

--- overseer.py
+++ overseer.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
 '''
-The overseer module is the module that actually handles the generation of the 
-functions, and orchestrates all the other functions. In the future, this 
-module will be the one that dispatches threads or worker nodes in a cluster 
-environment. 
+The overseer module is the module that actually handles the generation of the
+functions, and orchestrates all the other functions. In the future, this
+module will be the one that dispatches threads or worker nodes in a cluster
+environment.
+
+
+"--mca mpi_yield_when_idle 1" to stop busy-waiting of threads.
 
 Created on Jun 9, 2014
 
@@ -11,32 +15,40 @@ Created on Jun 9, 2014
 from __future__ import division
 from T_od import Point
 from itertools import combinations
-from generators import completions
-from comparitors import checkPartialDisjointness, checkCommutativity, checkSurjectivity
-from multiprocessing import Pool, Manager
-from threading import Thread
 from datetime import datetime
+from mpi4py import MPI
+from mpiGlobals import *
+from generators import completions
+from comparitors import checkPartialDisjointness
+from overseer_worker import main_worker
+
 
 #globals. These are important
 
-N = 5
+N = 4
 M = 3
 T = 3
-#filename to save results to
-FILENAME='mapping_results.txt'
-#maximum size of queue to report results to
-QUEUE_SIZE = 10000
-#number of mapping worker-processes
-NUM_THREADS = 3
-#whether to discard mappings which are not surjective
-CHECK_SURJECTIVITY=True
+#filename to write results to
+FILENAME = 'mapping_results.txt'
+#number of worker processes to spawn
+NUM_WORKERS = 3
+#check for surjectivity of maps before checking commutativity
+CHECK_SURJECTIVITY = True
+#worker spawn parameters
+WORKER_EXEC = 'python'
+WORKER_ARGV = ['overseer_mpi_worker.py']
+#how often workers report status updates back to the main process
+STATUS_UPDATE_INTERVAL = 1000000
+#amount to complete each map before sending to workers.
+PREWORKER_COMPLETION_LENGTH = 0
+
 
 def generate_basepoints():
     """
     Generate a list of possible basepoints.
-    
-    In order to make this approach easier to thread in the future, we can 
-    initially divide the sample space based on where the basepoint lies. 
+
+    In order to make this approach easier to thread in the future, we can
+    initially divide the sample space based on where the basepoint lies.
     In this way we can reduce the problem into (3M+1 choose 2) - M problems,
     each of which may be solved individually. This is a good start.
     """
@@ -47,120 +59,115 @@ def generate_basepoints():
     return basepoints
 
 
-def main():
-    man = Manager()
-    pairQueue = man.Queue(QUEUE_SIZE) # holds pairs which pass
-
-    basepoints = generate_basepoints()
-    #now construct an empty mapping for each basepoint
+def generate_pairs(basepoints):
+    '''
+    generate the list of pairs to send to the workers.
+    '''
+    #first generate some empty mappings
     empty_mappings = []
     for bp in basepoints:
         thisMap = [bp]
-        #add correct number of empty legs
         for i in xrange(T):
             thisMap.append(tuple())
-        empty_mappings.append(tuple(thisMap))
-    #now we have a list of empty maps
-    #now we can pull every combination of these and start the comparison.
-    #since two maps with the same basepoint are definitely going to fail the
-    #test, we can use the simple itertools.combinations() to get this list
-    empty_pairs = combinations(empty_mappings, 2)
-    
-    
-    #readerThread logs the results of the calculations
-    readerThread = Thread(target=result_reader, args=(pairQueue,))
-    readerThread.start()
-    
-    #since we only have a small number of cores, we probably 
-    #want to spawn a thread for every pair here.
-    pool = Pool(processes=NUM_THREADS)
-    #we need to pack q in with the iterable since that's how map works.
-    pool.map(pairWorker, iterable=( (pairQueue,pair) for pair in empty_pairs))
-    #now wait on the queue for things to be passed back
-    #once the pool has finished its work, send a sentinel value
-    #into the queue to tell the writer thread to stop what it's doing
-    pairQueue.put("DONE")
-    
-    #now join the readerThread
-    readerThread.join()
-    #and now we should be done.
+        empty_mappings.append(thisMap)
+    #now we need to pre-complete each map to the specified length.
+    #and return every possible combination of these partial mappings.
+    if PREWORKER_COMPLETION_LENGTH == 0:
+        return combinations(empty_mappings, 2)
+    #generate every completion
+    partialMaps = []
+    for emptyMap in empty_mappings:
+        for partial in completions(emptyMap, N, M, T,
+                                   length=PREWORKER_COMPLETION_LENGTH):
+            partialMaps.append(partial)
+    #now every combination. Throw away any pair which shares the same basepoint
+    partialPairs = []
+    for pair in combinations(partialMaps, 2):
+        m1, m2 = pair
+        if not checkPartialDisjointness(m1, m2, N, M, T):
+            continue
+        else:
+            partialPairs.append(pair)
+    return partialPairs.__iter__()
 
 
+def main_master(comm):
+    #spawn worker processes and establish an intercommunicator
+    num_workers = comm.Get_size()
 
-        
-
-def result_reader(q):
-    '''
-    Grabs the results of the pairWorker pool and writes results to both a file
-    and to stdout. 
-    @param q the queue to read results from
-    @param f the file to write to
-    '''
+    basepoints = generate_basepoints()
+    empty_pairs = generate_pairs(basepoints)
+    #Open file for writing
     f = open(FILENAME, 'a')
-    f.write("new test:: started: {}\n".format( str(datetime.now()) ))
+    #header
+    f.write("new test:: started: {}\n".format(str(datetime.now())))
+    print "new test:: started: {}\n".format(str(datetime.now()))
     f.flush()
-    result = q.get()
-    while result != "DONE":
-        print "FOUND ONE:"
-        print "\tmap 1: {} \n\tmap 2: {}".format(*result)
-        f.write("FOUND ONE:\n")
-        f.write("\tmap 1: {} \n\tmap 2: {}\n".format(*result))
+
+    #now make a list of what each process is doing.
+    #a value of True means that the process is currently doing work.
+    #a value of False means that it has been stopped.
+    status = [True for i in range(num_workers)]
+    #Now we need to send an initial pair to each worker.
+    for i in range(num_workers):
+        pair = empty_pairs.next()
+        print "Worker # {} starting pair: {}".format(i, pair)
+        f.write("Worker # {} starting pair: {}\n".format(i, pair))
         f.flush()
-        result = q.get()
+        comm.send(pair, dest=i, tag=TAG_WORKER_COMMAND)
+    #now we need to wait for one of the processes to report that it is
+
+    while True in status:
+        kind, result = comm.recv(source=MPI.ANY_SOURCE, tag=TAG_WORKER_REPORT)
+        #if we found a pair, send a new pair
+        if kind == REPORT_DONEPAIR:
+            try:
+                #send a new pair if one is available
+                newpair = empty_pairs.next()
+                print "Worker # {} starting pair: {}".format(result, newpair)
+                f.write("Worker # {} starting pair: {}\n".format(result, newpair))
+                f.flush()
+                comm.send(newpair, dest=result, tag=TAG_WORKER_COMMAND)
+            except StopIteration:
+                #if we have no pairs left to test, tell the process to stop
+                status[result] = False
+                comm.send(COMMAND_STOP, dest=result, tag=TAG_WORKER_COMMAND)
+        #log any reports of success
+        elif kind == REPORT_FOUNDPAIR:
+            result_writer(result, f)
+        elif kind == REPORT_STATUS:
+            #unpack and print a pretty message
+            workerRank, counts = result
+            print "Worker # {} status: {}".format(workerRank, counts)
+            f.write("Worker # {} status: {}\n".format(workerRank, counts))
+            f.flush()
+
+    #and finish the file.
     print 'finished checking :: time: {}'.format(str(datetime.now()))
-    f.write("finished checking :: time: {}\n".format(str(datetime.now())))
+    f.write("finished checking :: time: {}\n\n".format(str(datetime.now())))
     f.close()
-    return
 
-    
-def pairWorker(q_and_pair):
+    #and now we should be done.
+    comm.Barrier()
+    MPI.Finalize()
 
-    #now we have a pair of empty maps. At this point we should generate
-    #completions for each and test them against eachother.
-    #to speed up execution a bit, a disjointness check will be completed
-    #about halfway through generation, so that we do not waste resources 
-    #on maps that fail early.
-    
-    #we needed to pack the queue object with the pair to work with Pool.map() 
-    q, pair = q_and_pair
-    print "Starting pair: {}".format(pair)
-    empty1, empty2 = pair
-    for partial1 in completions(empty1, N, M, T, length=N//2):
-        for partial2 in completions(empty2, N, M, T, length=N//2):
-            #check partial disjointness, only proceed if true
-            if checkPartialDisjointness(partial1, partial2, N, M, T)==False:
-                continue
-            #if we pass the partial disjointness check at this point, then
-            #we may continue with this part of the generation
-            for map1 in completions(partial1, N, M, T):
-                #optionally skip a mapping if it is not surjective
-                if CHECK_SURJECTIVITY and not checkSurjectivity(map1, N, M, T):
-                    continue
-                for map2 in completions(partial2, N, M, T):
-                    if CHECK_SURJECTIVITY and not checkSurjectivity(map2, N, M, T):
-                        continue
-                    #now we should have two complete maps. We can now check
-                    #for both disjointness and commutivity
-                    #checkDisjointness is the faster function, so do it 
-                    #first
-                    if checkPartialDisjointness(map1, map2, N, M, T)==True:
-                        #debug bypass the commutivity checker---------------V
-                        if checkCommutativity(map1, map2, N, M, T)==True:
-                            #now we have found a pair which passes the test.
-                            #append it to the good list.
-                            q.put((map1, map2))
 
-    #now we're done.
-                        
-                    
-                
-        
+def result_writer(result, f):
+    '''
+    commits a pair to the log
+    '''
+
+    print "FOUND ONE:"
+    print "\tmap 1: {} \n\tmap 2: {}".format(*result)
+    f.write("FOUND ONE:\n")
+    f.write("\tmap 1: {} \n\tmap 2: {}\n".format(*result))
+    f.flush()
 
 if __name__ == "__main__":
-    main()
-    
-    
-    
-    
-#TODO: don't make so many new variables. 
-    
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    if rank == 0:
+        main_master()
+    else:
+        main_worker()
+#TODO: don't make so many new variables.
